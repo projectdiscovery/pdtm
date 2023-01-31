d43a0855efd02c91026fbebc95207cd4bc7633ff
xm1k3 <xm1k3@protonmail.com>
2023-01-31 09:12:04 +0100

    better logic
    If the tool exists, then it will be removed.

--- pkg/remove.go
+++ pkg/remove.go
@@ -2,32 +2,23 @@ package pkg
 
 import (
 	"os"
-	"os/exec"
-
-	stringsutil "github.com/projectdiscovery/utils/strings"
+	"path/filepath"
 
 	"github.com/projectdiscovery/gologger"
 )
 
 // Remove removes given tool
-func Remove(tool Tool) error {
-	executablePath, err := exec.LookPath(tool.Name)
-	if err != nil {
-		return err
-	}
-
-	// Check if the path of the executable is in a system directory
-	systemDirs := []string{"/usr/bin", "/usr/local/bin", "/bin", "/snap/bin", "/opt/homebrew/bin", "C:/Program Files", "C:/Program Files (x86)"}
-	if stringsutil.ContainsAny(executablePath, systemDirs...) {
-		gologger.Info().Msgf("skipping removal of %s...", tool.Name)
+func Remove(tool Tool, path string) error {
+	executablePath := filepath.Join(path, tool.Name)
+	if _, err := os.Stat(executablePath); err == nil {
+		gologger.Info().Msgf("removing %s...", tool.Name)
+		err = os.Remove(executablePath)
+		if err != nil {
+			return err
+		}
+		gologger.Info().Msgf("removed %s", tool.Name)
 		return nil
 	}
-
-	gologger.Info().Msgf("removing %s...", tool.Name)
-	err = os.Remove(executablePath)
-	if err != nil {
-		return err
-	}
-	gologger.Info().Msgf("removed %s", tool.Name)
+	gologger.Info().Msgf("skipping removal of %s...", tool.Name)
 	return nil
 }
