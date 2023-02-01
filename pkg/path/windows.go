e798848d45c02d8596db44df15a9326ac4633b6f
xm1k3 <xm1k3@protonmail.com>
2023-02-01 15:06:24 +0100

    add env var as suffix windows

--- pkg/path/windows.go
+++ pkg/path/windows.go
@@ -35,7 +35,7 @@ func add(p string) (bool, error) {
 		return false, nil
 	}
 
-	cur = append([]string{p}, cur...)
+	cur = append(cur, p)
 	err = write(p, cur)
 	if nil != err {
 		return false, err
