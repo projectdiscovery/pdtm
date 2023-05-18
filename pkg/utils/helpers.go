887c93131812ee45251c7e03aad86fb6881e4306
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-05-18 19:24:33 +0530

    skip update on error

--- pkg/utils/helpers.go
+++ pkg/utils/helpers.go
@@ -28,7 +28,8 @@ func GetUpdaterCallback(toolName string) func() {
 		dp := filepath.Join(home, ".pdtm/go/bin")
 		tool, err := fetchTool(toolName)
 		if err != nil {
-			gologger.Error().Msg(err.Error())
+			gologger.Error().Msgf("failed to fetch details of %v skipping update: %v", toolName, err)
+			return
 		}
 		err = pkg.Update(dp, tool)
 		if err == types.ErrIsUpToDate {
