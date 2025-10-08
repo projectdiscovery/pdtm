ab9d1117e9c64e5d80dd044b730af316f244f770
Doğan Can Bakır <dogancanbakir@protonmail.com>
2025-10-08 11:22:29 +0300

    fix version checking

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -15,20 +15,40 @@ var RegexVersionNumber = regexp.MustCompile(`(?m)[v\s](\d+\.\d+\.\d+)`)
 
 func ExtractInstalledVersion(tool types.Tool, basePath string) (string, error) {
 	toolPath := filepath.Join(basePath, tool.Name)
-	cmd := exec.Command(toolPath, "--version")
 
-	var outb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &outb
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
+	versionCommands := []string{"--version", "version"}
 
-	if installedVersion := RegexVersionNumber.FindString(strings.ToLower(outb.String())); installedVersion != "" {
-		installedVersionString := strings.TrimPrefix(strings.TrimSpace(installedVersion), "v")
-		return installedVersionString, nil
+	for _, versionCmd := range versionCommands {
+		if version, err := tryVersionCommand(toolPath, versionCmd); err == nil {
+			return version, nil
+		}
 	}
 
 	return "", errors.New("unable to extract installed version")
+}
+
+func tryVersionCommand(toolPath, versionCmd string) (string, error) {
+	cmd := exec.Command(toolPath, versionCmd)
+	var outb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &outb
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	output := outb.String()
+	if output == "" {
+		return "", errors.New("empty output")
+	}
+
+	installedVersion := RegexVersionNumber.FindString(strings.ToLower(output))
+	if installedVersion == "" {
+		return "", errors.New("no version found in output")
+	}
+
+	version := strings.TrimSpace(installedVersion)
+	version = strings.TrimPrefix(version, "v")
+
+	return version, nil
 }
